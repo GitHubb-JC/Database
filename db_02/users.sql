7ee9b5234159777f84cf320184bfb24a6478bbfb
JC <chohy1106@gmail.com>
2022-08-17 16:24:07 +0900

    0817

---
+++ db_02/users.sql
@@ -0,0 +1,10 @@
+-- SQLite
+create table users (
+    first_name text not null,
+    last_name text not null,
+    age integer not null,
+    country text not null,
+    phone text not null,
+    balance integer not null
+);
+
